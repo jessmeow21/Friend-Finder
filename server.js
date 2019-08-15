62124b81bcf88b001cd0c4f99ca2a383f2c9e4de
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-08-15 10:33:05 -0400

    github link

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
