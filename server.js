2a94f4c64fccd3b9012f77973ddefb1b5a7f3e96
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-08-16 10:24:04 -0400

    connection update

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-const PORT = 3000;
-
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
