bffb7d3d4f94d61e0351f84b0bc60f966f1a436d
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-08-16 09:58:39 -0400

    updated friends js

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+//create an array of objects
+var friends = [
+    {
+        name: "Steven Wong",
+        photo: "https://pbs.twimg.com/profile_images/625769159339737088/2dwpQAXA.jpg",
+        scores: [5,1,2,3,5,2,1,2,4,2]
+    },{
+        name: "Britney Spears",
+        photo: "https://pbs.twimg.com/profile_images/665552260357824513/4BF9Zjiq_400x400.jpg",
+        scores: [3,1,4,2,3,4,2,1,3,2]
+    },{
+        name: "Christina Aguilera",
+        photo: "https://ichef.bbci.co.uk/images/ic/256x256/p01bqjvg.jpg",
+        scores: [1,2,3,4,1,2,3,4,5,1]
+    }, {
+        name: "Tom Holland",
+        photo: "https://i.pinimg.com/236x/8e/54/5c/8e545c853ebb86c5b87fdfa186a646f9.jpg",
+        scores: [5,4,3,2,1,5,4,3,2,1]
+    }, {
+        name: "Captain America",
+        photo: "https://pbs.twimg.com/profile_images/685896589362216963/N2j7Rc9E.png",
+        scores: [2,3,4,5,2,3,4,5,1,2]
+    },{
+        name: "Iron Man",
+        photo: "https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/21832/square_thumb%402x.jpg",
+        scores: [1,2,3,4,5,1,2,4,2,2]
+    }
+];
+//json database is ready
+module.exports = friends;
